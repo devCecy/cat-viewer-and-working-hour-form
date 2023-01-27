47a9d1df276b016a6cba9352b80a1604913aea38
devCecy <im.ceciliaan@gmail.com>
2023-01-27 14:03:31 +0900

    feat : navigation style

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,10 +6,10 @@ const Navigation = () => {
 		<Container>
 			<ul>
 				<li>
-					<Link to="/cat-viewer">CatViewer</Link>
+					<Link to="/cat-viewer">Cat Viewer</Link>
 				</li>
 				<li>
-					<Link to="/working-hour">WorkingHours</Link>
+					<Link to="/working-hour">Working Hours</Link>
 				</li>
 			</ul>
 		</Container>
@@ -21,5 +21,26 @@ export default Navigation;
 const Container = styled.nav`
 	position: fixed;
 	top: 0;
-	left: 0;
+	width: 100%;
+	z-index: 999;
+
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	padding: 2rem 0;
+	width: 100vw;
+
+	ul {
+		display: flex;
+		justify-content: space-between;
+		column-gap: 1rem;
+
+		li {
+			padding: 1rem;
+			font-size: 1.5rem;
+			border-radius: 0.5rem;
+			color: white;
+			background-color: ${({ theme }) => theme.colors.primary};
+		}
+	}
 `;
