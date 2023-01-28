7a3a90d59a7b15e1edcb6af88f6fbc797e85e5a4
devCecy <im.ceciliaan@gmail.com>
2023-01-28 23:50:39 +0900

    feat : 환경변수 생성

---
+++ src/utils/envirnment.ts
@@ -0,0 +1,5 @@
+// export const CAT_API_URL = process.env.REACT_APP_CAT_API_URL;
+// export const CAT_API_KEY = process.env.REACT_APP_CAT_API_KEY;
+export const CAT_API_URL = "https://api.thecatapi.com/v1";
+export const CAT_API_KEY =
+	"live_k77YJ1Sa3RsUfqEwbuKzrsevPSBW7iCoeeTZKSuj0ahl51TyYwbMXoLhVwwyIIvF";
