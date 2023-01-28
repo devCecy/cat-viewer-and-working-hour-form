33cd676dc8bbdedbce8d36d165457bd3aebeb282
devCecy <im.ceciliaan@gmail.com>
2023-01-28 23:54:45 +0900

    feat : recoil, react-query client추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import App from "./App";
+import { RecoilRoot } from "recoil";
+
+// styles
 import GlobalStyle from "./styles/global";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme";
+
+// react-query
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement
@@ -12,7 +18,11 @@ const root = ReactDOM.createRoot(
 root.render(
 	<React.StrictMode>
 		<ThemeProvider theme={theme}>
-			<App />
+			<RecoilRoot>
+				<QueryClientProvider client={queryClient}>
+					<App />
+				</QueryClientProvider>
+			</RecoilRoot>
 		</ThemeProvider>
 		<GlobalStyle />
 	</React.StrictMode>
