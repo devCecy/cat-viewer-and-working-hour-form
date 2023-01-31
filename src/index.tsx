a111a6abc74a6ac2ef4ed94c29802d0313c5ea6f
devCecy <im.ceciliaan@gmail.com>
2023-01-31 21:04:55 +0900

    feat : store생성

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,10 @@ import GlobalStyle from "./styles/global";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme";
 
+// redux
+import store from "./redux/store";
+import { Provider } from "react-redux";
+
 // react-query
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
@@ -18,11 +22,13 @@ const root = ReactDOM.createRoot(
 root.render(
 	<React.StrictMode>
 		<ThemeProvider theme={theme}>
-			<RecoilRoot>
-				<QueryClientProvider client={queryClient}>
-					<App />
-				</QueryClientProvider>
-			</RecoilRoot>
+			<Provider store={store}>
+				<RecoilRoot>
+					<QueryClientProvider client={queryClient}>
+						<App />
+					</QueryClientProvider>
+				</RecoilRoot>
+			</Provider>
 		</ThemeProvider>
 		<GlobalStyle />
 	</React.StrictMode>
