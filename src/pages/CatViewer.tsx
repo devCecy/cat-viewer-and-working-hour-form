9527926eaf40411810b75de3ebfe3e00e45dcb69
devCecy <im.ceciliaan@gmail.com>
2023-02-02 21:28:15 +0900

    feat : 스케일 애니메이션

--- src/pages/CatViewer.tsx
+++ src/pages/CatViewer.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+import Masonry from "react-masonry-css";
 
 // api
 import { useQuery } from "@tanstack/react-query";
@@ -12,11 +13,19 @@ interface CatImgResult {
 	url: string;
 }
 
+interface ImgRectProps {
+	width: number;
+	height: number;
+	top: number;
+	left: number;
+}
+
 const CatViewer = () => {
 	const { ref, inView } = useInView();
 	const [catImgs, setCatImgs] = useState<CatImgResult[]>([]);
 	const [currentPage, setCurrentPage] = useState(0);
 	const [selectedImg, setSelectedImg] = useState("");
+	const [isImgClicked, setIsImgClicked] = useState(false);
 
 	const PER_PAGE = 30;
 	const fetchGroups = (): Promise<CatImgResult[]> =>
@@ -47,30 +56,78 @@ const CatViewer = () => {
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [currentPage]);
 
+	const [imgRect, setImgRect] = useState<ImgRectProps>({
+		width: 0,
+		height: 0,
+		top: 0,
+		left: 0,
+	});
+
 	/**
 	 * 클릭된 이미지를 크게 보여줍니다.
 	 */
 	const handleImgModal = (e: React.MouseEvent<HTMLDivElement>) => {
 		setSelectedImg(e.currentTarget.id);
+
+		const targetElement = document.getElementById(e.currentTarget.id);
+		if (!targetElement) return;
+		const imgRect = targetElement.getBoundingClientRect();
+
+		setImgRect({
+			width: Math.floor(imgRect.width),
+			height: Math.floor(imgRect.height),
+			top: Math.floor(imgRect.top),
+			left: Math.floor(imgRect.left),
+		});
+		setIsImgClicked(true);
 	};
 
+	const breakpointColumnsObj = {
+		default: 3,
+		1200: 3,
+		700: 2,
+		500: 1,
+	};
 	return (
 		<>
 			<Container>
 				<Contents>
-					{catImgs.map((img) => {
-						return (
-							<ImgBox
-								props={selectedImg}
-								key={`${currentPage}-${img.id}`}
-								id={img.url}
-								onClick={handleImgModal}
-							>
-								<img src={img.url} id={img.url} alt="" />
-							</ImgBox>
-						);
-					})}
+					<Masonry
+						breakpointCols={breakpointColumnsObj}
+						className="list"
+						columnClassName="column"
+					>
+						{catImgs.map((img) => {
+							return (
+								<ImgBox
+									props={selectedImg}
+									key={`${currentPage}-${img.id}`}
+									id={img.url}
+									onClick={handleImgModal}
+								>
+									<img src={img.url} id={img.url} alt="" />
+								</ImgBox>
+							);
+						})}
+					</Masonry>
 				</Contents>
+
+				{isImgClicked ? (
+					<ImgModal
+						props={imgRect}
+						onClick={() => setIsImgClicked(false)}
+						src={selectedImg}
+						id={selectedImg}
+						alt=""
+					/>
+				) : (
+					<CloseImgModal
+						props={imgRect}
+						src={selectedImg}
+						id={selectedImg}
+						alt=""
+					/>
+				)}
 			</Container>
 
 			{/* 마지막 contents */}
@@ -87,32 +144,30 @@ const Container = styled.section`
 `;
 
 const Contents = styled.div`
-	column-gap: 1.6rem;
-	column-count: 3;
-	margin: 0 2.4rem;
-
-	@media screen and (max-width: 500px) {
-		margin: 0 1.6rem;
-		column-count: 1;
+	.list {
+		display: flex;
+		padding: 20px 10px;
 	}
 
-	@media screen and (min-width: 500px) and (max-width: 767px) {
-		column-count: 2;
+	.column {
+		padding: 0 1rem;
 	}
 `;
 
 const ImgBox = styled.div<{ props: string }>`
-	margin-bottom: 15px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	margin: 1.6rem 0;
 
 	img {
 		width: 100%;
 		max-width: 100%;
 		height: 100%;
 		cursor: pointer;
-		transition: transform 0.2s;
 	}
 
-	&:active img {
+	/* &:active img {
 		position: fixed;
 		top: 0;
 		left: 0;
@@ -124,7 +179,59 @@ const ImgBox = styled.div<{ props: string }>`
 		-ms-transform: scale(1.1);
 		-o-transform: scale(1.1);
 		transition: transform 0.2s;
+	} */
+`;
+
+const scaleUp = (props: ImgRectProps) => keyframes`
+  0% {
+  top: ${Number(props.top) + Number(props.height) / 2}px;
+	left: ${Number(props.left) + Number(props.width) / 2}px;
+	width:${props.width}px;
+	height: ${props.height}px;
+    }
+	100%{
+	top: 50%;
+	left: 50%;
+	width: 100vw;
+	height: 100vh;
 	}
+`;
+
+const ImgModal = styled.img<{ props: ImgRectProps }>`
+	position: fixed;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	width: 100vw;
+	height: 100vh;
+
+	animation: ${(props) => scaleUp(props.props)} 0.3s linear;
+`;
+
+const scaleDown = (props: ImgRectProps) => keyframes`
+  0% {
+	top: 50%;
+	left: 50%;
+	width:100%;
+	height: 100vh;
+  }
+	100%{
+  top: ${Number(props.top) + Number(props.height) / 2}px;
+	left: ${Number(props.left) + Number(props.width) / 2}px;
+	width: 0;
+	height: 0;
+	}
+`;
+
+const CloseImgModal = styled.img<{ props: ImgRectProps }>`
+	position: fixed;
+	top: ${(props) => Number(props.props.top) + Number(props.props.height) / 2}px;
+	left: ${(props) =>
+		Number(props.props.left) + Number(props.props.width) / 2}px;
+	width: 0;
+	height: 0;
+	transform: translate(-50%, -50%);
+	animation: ${(props) => scaleDown(props.props)} 0.3s linear;
 `;
 
 const Loading = styled.div`
